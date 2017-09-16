a822ade18385248e39d566c4ca21bb0d0222fbec
Jazerzxc <james992927108@gmail.com>
2017-09-16 13:42:59 +0800

    finish p924

---
+++ p924/cmake-build-debug/CMakeFiles/p924.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p924.dir/main.cpp.o"
+  "p924.pdb"
+  "p924"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p924.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
