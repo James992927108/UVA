dbf72e28d20538960b60261726e2d555736b6a08
James992927108 <james992927108@gmail.com>
2017-09-22 02:24:00 +0800

    version 0.0.1

--- p10110/cmake-build-debug/CMakeFiles/p10110.dir/cmake_clean.cmake
+++ p10110/cmake-build-debug/CMakeFiles/p10110.dir/cmake_clean.cmake
@@ -1,7 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/p10110.dir/main.cpp.o"
+  "CMakeFiles/p10110.dir/main.cpp.obj"
   "p10110.pdb"
-  "p10110"
+  "p10110.exe"
+  "p10110.exe.manifest"
+  "libp10110.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
